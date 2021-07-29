0f25690b0b1f3a778ecbec18cff08fa7be0bc981
Vasile Rotaru <vasilerotaru38@gmail.com>
2021-07-29 16:12:40 +0300

    The type of training has been changed to continuous.

--- PF-AFN_train/scripts/train_PBAFN_stage1.sh
+++ PF-AFN_train/scripts/train_PBAFN_stage1.sh
@@ -1,5 +1,6 @@
 python -m torch.distributed.launch --nproc_per_node=1 train_PBAFN_stage1.py --name PBAFN_stage1 \
---resize_or_crop None --verbose --tf_log --batchSize 4 --num_gpus 1 --label_nc 14 --launcher pytorch
+--resize_or_crop None --verbose --tf_log --batchSize 4 --num_gpus 1 --label_nc 14 --launcher pytorch \
+--continue_train
 
 
 
